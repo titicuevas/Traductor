cf8851b940cc964112c4f2ab263b43d39469a51b
titicuevas <enrique.cuevas@iesdonana.org>
2024-10-22 13:47:23 +0200

    Actualizacion de ficheros. Cambio en diseño y añadido el debounce para mejorar la traduccion

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -1,48 +1,65 @@
-import { useState, useEffect } from 'react';
-import { translateText } from '../lib/libreTranslate';
-import { LanguageSelector } from './LanguageSelector';
-import '../App.css';
+import { useState, useEffect } from 'react'
+import { translateText } from '../lib/libreTranslate'
+import { LanguageSelector } from './LanguageSelector'
+import '../App.css'
+
+// Función debounce fuera del componente
+const debounce = (func, delay) => {
+  let debounceTimeout
+  return (...args) => {
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout)
+    }
+    debounceTimeout = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
 
 const Translator = () => {
-  const [inputText, setInputText] = useState('');
-  const [translatedText, setTranslatedText] = useState('');
-  const [sourceLanguage, setSourceLanguage] = useState('EN');
-  const [targetLanguage, setTargetLanguage] = useState('ES');
+  const [inputText, setInputText] = useState('')
+  const [translatedText, setTranslatedText] = useState('')
+  const [sourceLanguage, setSourceLanguage] = useState('EN')
+  const [targetLanguage, setTargetLanguage] = useState('ES')
 
-  // Traduce el texto cada vez que el texto de entrada, el idioma de origen o el idioma de destino cambian.
+  // Almacena la función debouncedHandleTranslate fuera del useEffect
+  const debouncedHandleTranslate = debounce(async (text) => {
+    if (text) {
+      const translation = await translateText(text, targetLanguage, sourceLanguage)
+      setTranslatedText(translation)
+    } else {
+      setTranslatedText('')
+    }
+  }, 500)
+
+  // Llamada a la traducción cuando cambia el inputText, sourceLanguage o targetLanguage
   useEffect(() => {
-    const handleTranslate = async () => {
-      if (inputText) {
-        const translation = await translateText(inputText, targetLanguage, sourceLanguage);
-        console.log(`Translation result: ${translation}`);
-        setTranslatedText(translation);
-      } else {
-        setTranslatedText('');
-      }
-    };
-    handleTranslate();
-  }, [inputText, sourceLanguage, targetLanguage]);
+    debouncedHandleTranslate(inputText)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputText, sourceLanguage, targetLanguage])
 
-  // Intercambia los idiomas y traduce el texto de entrada en el nuevo idioma.
+  // Función para intercambiar los idiomas
   const handleSwapLanguages = async () => {
-    // Intercambia los idiomas
-    const tempSource = sourceLanguage;
-    const tempTarget = targetLanguage;
+    const tempSource = sourceLanguage
+    const tempTarget = targetLanguage
 
-    // Intercambia los idiomas en los estados
-    setSourceLanguage(tempTarget);
-    setTargetLanguage(tempSource);
+    setSourceLanguage(tempTarget)
+    setTargetLanguage(tempSource)
 
-    // Traduce el texto de entrada con los nuevos idiomas
-    const newTranslation = await translateText(inputText, tempTarget, tempSource);
-    setInputText(newTranslation); // Establece el texto de entrada al texto traducido
-    setTranslatedText(inputText); // Establece el texto traducido al texto original
-  };
+    // Traducir el texto actual con los nuevos idiomas
+    if (inputText) {
+      const newTranslation = await translateText(inputText, tempTarget, tempSource)
+      setInputText(newTranslation) // Actualiza el texto de entrada con la nueva traducción
+      setTranslatedText('') // Limpiar el texto traducido para evitar confusión
+    } else {
+      setTranslatedText('') // Limpia el texto traducido si no hay entrada
+    }
+  }
 
   return (
     <div className="translator-container">
-      <h1>Traductor</h1>
-      <div className="row justify-content-center align-items-center">
+      <h1 className="text-center mb-4">Traductor</h1>
+      <div className="row">
         <div className="col-md-5 mb-3 text-area-container">
           <LanguageSelector
             language={sourceLanguage}
@@ -54,7 +71,6 @@ const Translator = () => {
             value={inputText}
             onChange={(e) => setInputText(e.target.value)}
             placeholder="Ingresa el texto a traducir"
-            style={{ resize: 'none' }}
           />
         </div>
         <div className="col-md-2 text-center">
@@ -73,12 +89,11 @@ const Translator = () => {
             value={translatedText}
             readOnly
             placeholder="Texto traducido aparecerá aquí"
-            style={{ resize: 'none' }}
           />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Translator;
+export default Translator
